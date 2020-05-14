c8ceb8f7acd95dcc5ced3413fab04b2c9165e187
Nichols <jnicamzn@a483e7ad3e54.ant.amazon.com>
2020-05-14 08:46:19 -0700

    Add create cluster shell

---
+++ utils/create-cluster.sh
@@ -0,0 +1,1 @@
+nohup eksctl create cluster --name=kf-sm-workshop --version=1.15 --nodes=6 --managed --alb-ingress-access --region=us-west-2 > kf-sm-workshop-eksctl2.out 2> kf-sm-workshop-eksctl2.err
