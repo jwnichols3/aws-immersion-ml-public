44560de0d594d0c5b31d5b3f02227ff07ef3b32a
Nichols <jnicamzn@a483e7ad3e54.ant.amazon.com>
2020-05-18 12:55:54 -0700

    Add annotations Remove sudo yum update -y from launch

--- utils/sm-start.sh
+++ utils/sm-start.sh
@@ -2,26 +2,30 @@
 
 # yum -y update
 
+echo "STEP Installing Docker"
 sudo yum -y install docker
 
 # nohup pip install --upgrade pip > pip-upgrade.out 2> pip-upgrade.err &
-
+echo "STEP Installing kubectl"
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
 
 chmod +x ./kubectl
 
 sudo mv -v ./kubectl /usr/local/bin
 
+echo "STEP Installing kfctl"
 curl --silent --location "https://github.com/kubeflow/kfctl/releases/download/v1.0.2/kfctl_v1.0.2-0-ga476281_linux.tar.gz" | tar xz -C /tmp
 
 sudo mv -v /tmp/kfctl /usr/local/bin
 
 # May 14, 2020: Defaulting to 0.18 as 0.19 caused errors
 # curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+echo "STEP Installing eksctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/0.18.0/eksctl_Linux_amd64.tar.gz" | tar xz -C /tmp
 
 sudo mv -v /tmp/eksctl /usr/local/bin
 
+echo "STEP installing aws-iam-authenticator"
 curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
 
 sudo chmod +x ./aws-iam-authenticator
